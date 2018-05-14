be31fb4ff499a71b3671bddd55e7e256096561eb
nyhilo <silentclowd@gmail.com>
2018-05-13 18:06:12 -0700

    Lerning how to work with channels

--- thebot.py
+++ thebot.py
@@ -5,30 +5,63 @@ def init():
     import discord
     from discord.ext import commands
     import sys
+
+    # Local imports
     import point_management
     import events
 
-    # Vars
+    # Vars #
+    PREFIX = '!'
+
+    # Bot should only have read/write permission to these channels
+    class CHANNEL:
+        inventory = 'turn-order'
+        controls = 'bot-control'
+
+    # Format: [Username, Points, {Inventory Dict}]
+    playerlist = [  
+
+                    ]
+
+
     with open('token.txt','r') as file:
         TOKEN = file.readline()
 
-    bot = commands.Bot(command_prefix='!', description="A bot to manage a whole bunch of things on the Now We Nomic Server")
+    client = commands.Bot(command_prefix=PREFIX, description="A bot to manage a whole bunch of things on the Now We Nomic Server")
 
     # Bot Functions #
-    @bot.event
+    @client.event
     async def on_ready():
         print("Python version", sys.version)
-        print('Logged in as', bot.user.name)
+        print('Logged in as', client.user.name)
         print("Discord API version: " + discord.__version__)
+        await client.change_presence(game=discord.Game(name='Nomic'))
+        await client.send_message(client.get_channel('bot-control'), 'I live!') # <- How to find channel object
+        # await client.send_message(discord.Object(CHANNEL.controls), 'I live!')
 
 
-    @bot.command()
+    @client.command()
     async def hello():
-        bot.msg = await bot.say('Hello World! I am Nwn-bot. Pleased to meet you!')
+        client.msg = await client.say('Hello World! I am Nwn-bot. Pleased to meet you!')
 
-    bot.run(TOKEN)
+    @client.command(pass_context=True)
+    async def greet(ctx):
+        print(ctx.message, ctx.message.channel)
+        await client.send_message(ctx.message.channel, "Say hello")
+        reply = await client.wait_for_message(author=ctx.message.author, content="hello")   # <- regex goes here
+        await client.send_message(ctx.message.channel, "Yo.")
 
-    print("Hello Bot")
+
+    # # Catch-all for messages.
+    # @client.event
+    # async def on_message(message):
+    #     if message.content.startswith(PREFIX + 'greet'):
+    #         await client.send_message(message.channel, 'Say hello')
+    #         msg = await client.wait_for_message(author=message.author, content='hello')
+    #         await client.send_message(message.channel, 'Hello.')
+    #     print(message.author + ": " + message.content)
+
+    client.run(TOKEN)
 
 if __name__ == '__main__':
     print("Please run this bot using init.py.")
