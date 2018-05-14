9b15d2bb165a537165d04743ba70066beaa4305a
nyhilo <silentclowd@gmail.com>
2018-05-13 18:24:38 -0700

    Started player tracking, channel referencing works

--- thebot.py
+++ thebot.py
@@ -12,22 +12,20 @@ def init():
 
     # Vars #
     PREFIX = '!'
-
-    # Bot should only have read/write permission to these channels
-    class CHANNEL:
-        inventory = 'turn-order'
-        controls = 'bot-control'
-
-    # Format: [Username, Points, {Inventory Dict}]
-    playerlist = [  
-
-                    ]
-
+    client = commands.Bot(command_prefix=PREFIX, description="A bot to manage a whole bunch of things on the Now We Nomic Server")
 
     with open('token.txt','r') as file:
         TOKEN = file.readline()
 
-    client = commands.Bot(command_prefix=PREFIX, description="A bot to manage a whole bunch of things on the Now We Nomic Server")
+    # Bot should only have read/write permission to these channels
+    class CHANNEL:
+        inventory = '445373512471805953'
+        controls = '445377007547580424'
+
+    # Format: [Username, Points, {Inventory Dict}]
+    playerlist = [ ["Nyhilo#0896", 10, {"Laser Beam":1, "Bowl of Peanuts":57}],
+                   ["mishka1980#4051", 1, {"Cookie":14}] ]
+
 
     # Bot Functions #
     @client.event
@@ -36,30 +34,20 @@ def init():
         print('Logged in as', client.user.name)
         print("Discord API version: " + discord.__version__)
         await client.change_presence(game=discord.Game(name='Nomic'))
-        await client.send_message(client.get_channel('bot-control'), 'I live!') # <- How to find channel object
-        # await client.send_message(discord.Object(CHANNEL.controls), 'I live!')
 
-
+    # Example command
     @client.command()
     async def hello():
         client.msg = await client.say('Hello World! I am Nwn-bot. Pleased to meet you!')
+        await client.send_message(client.get_channel(CHANNEL.controls), 'I live!')
 
+    # Example Call and Response
     @client.command(pass_context=True)
     async def greet(ctx):
         print(ctx.message, ctx.message.channel)
         await client.send_message(ctx.message.channel, "Say hello")
         reply = await client.wait_for_message(author=ctx.message.author, content="hello")   # <- regex goes here
         await client.send_message(ctx.message.channel, "Yo.")
-
-
-    # # Catch-all for messages.
-    # @client.event
-    # async def on_message(message):
-    #     if message.content.startswith(PREFIX + 'greet'):
-    #         await client.send_message(message.channel, 'Say hello')
-    #         msg = await client.wait_for_message(author=message.author, content='hello')
-    #         await client.send_message(message.channel, 'Hello.')
-    #     print(message.author + ": " + message.content)
 
     client.run(TOKEN)
 
